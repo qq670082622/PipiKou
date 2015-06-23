6f310948c3db08c87679a45104091fa508dc194f
吴铭 <670082622@qq.com>
2015-06-23 13:44:13 +0800

    no message

--- ShouKeBao/ShouKebao/HomeModel/yesterDayModel.h
+++ ShouKeBao/ShouKebao/HomeModel/yesterDayModel.h
@@ -30,6 +30,7 @@
 
 @property (nonatomic,copy) NSString *IsOffLine;// 是否离线
 @property (nonatomic,copy) NSString *HistoryViewTime;// 历史流浪时间
+@property (nonatomic,copy) NSString *PushDate;
 @property (nonatomic,strong) NSDictionary *ShareInfo;
 + (instancetype)modalWithDict:(NSDictionary *)dict;
 - (instancetype)initWithDict:(NSDictionary *)dict;
