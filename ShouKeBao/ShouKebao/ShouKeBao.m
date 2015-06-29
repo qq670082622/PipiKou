d1b841fa7dc3d9e124494f4f0d4e9c2e65506f16
吴铭 <670082622@qq.com>
2015-06-29 17:53:06 +0800

    no message

--- ShouKeBao/ShouKebao/ShouKeBao.m
+++ ShouKeBao/ShouKebao/ShouKeBao.m
@@ -361,7 +361,7 @@
 { //arr[0]是value arr[1]是key
     //orderId ,userId ,recommond ,productId ,messageId
    
-     [self getVoice];
+    // [self getVoice];
     [self headerPull];
     [self  getUserInformation];
    
