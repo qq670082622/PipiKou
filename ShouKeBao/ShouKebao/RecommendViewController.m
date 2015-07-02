be7a92c761f1a6dca4db18fa4edbeac72c61c6a6
冯坤 <928572909@qq.com>
2015-07-02 16:32:07 +0800

    no message

--- ShouKeBao/ShouKebao/RecommendViewController.m
+++ ShouKeBao/ShouKebao/RecommendViewController.m
@@ -421,7 +421,7 @@
                                 
                                 if (state == SSResponseStateSuccess)
                                 {
-                                    
+                                    //近期推荐
                                     [MBProgressHUD showSuccess:@"分享成功"];
                                     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.8 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{ // 2.0s后执行block里面的代码
                                         [MBProgressHUD hideHUD];
