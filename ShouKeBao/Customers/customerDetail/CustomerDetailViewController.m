66cca4fc9f0dc11665d468efb1c37d03f792d305
zhangzhengmei <13478644835@163.com>
2015-08-25 19:29:56 +0800

    定位 QQbug修改

--- ShouKeBao/Customers/customerDetail/CustomerDetailViewController.m
+++ ShouKeBao/Customers/customerDetail/CustomerDetailViewController.m
@@ -210,6 +210,8 @@
 {
     return 10.0f;
 }
+
+
 -(void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex{
     if (buttonIndex == 0) {
         NSString * telStr = [NSString stringWithFormat:@"tel://%@", self.tele.text];
@@ -241,11 +243,7 @@
                     [ale show];
                 }else if([[UIApplication sharedApplication]canOpenURL:[NSURL URLWithString:@"weixin://"]] && ![self.weChat.text isEqualToString:@""]){
                     [[UIApplication sharedApplication]openURL:[NSURL URLWithString:@"weixin://"]];
-
-                    
                 }
-
-         
                 
 //                if ([[UIApplication sharedApplication]canOpenURL:[NSURL URLWithString:@"weixin://"]] && ![self.weChat.text isEqualToString:@""]) {
 //                    [[UIApplication sharedApplication]openURL:[NSURL URLWithString:@"weixin://"]];
@@ -255,15 +253,18 @@
             case 2:
             {
                 if ([self.QQ.text isEqualToString:@""]) {
-                    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"提示" message:@"QQ号码为空！" delegate:self cancelButtonTitle:nil otherButtonTitles:@"确定", nil];
+                    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"提示" message:@"QQ号码为空!" delegate:nil cancelButtonTitle:nil otherButtonTitles:@"确定", nil];
                     [alert show];
                 }
                 if (![self joinGroup:nil key:nil] && ![self.QQ.text isEqualToString:@""]) {
-                    UIAlertView*ale=[[UIAlertView alloc] initWithTitle:@"提示" message:@"您没有安装手机QQ，请安装手机QQ后重试，或用PC进行操作。" delegate:self cancelButtonTitle:nil otherButtonTitles:@"确定", nil];
+                    UIAlertView *ale = [[UIAlertView alloc] initWithTitle:@"提示" message:@"您没有安装手机QQ，请安装手机QQ后重试，或用PC进行操作。" delegate:nil cancelButtonTitle:nil otherButtonTitles:@"确定", nil];
                     [ale show];
                 }
-
             }
+    
+                
+                
+                
                 break;
 
             default:
