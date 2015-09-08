8d07c49c79c423325e6f2888d826b950839fae0a
zhangzhengmei <13478644835@163.com>
2015-09-08 18:19:01 +0800

    修改搜索

---
+++ ShouKeBao/Customers/customerOrders/SearchView.h
@@ -0,0 +1,13 @@
+//
+//  SearchView.h
+//  ShouKeBao
+//
+//  Created by 张正梅 on 15/9/7.
+//  Copyright (c) 2015年 shouKeBao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchView : UIView
+@property (nonatomic,strong) UITableView *TableView;
+@end
