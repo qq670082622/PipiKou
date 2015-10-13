7163676c049b9f221ac92d881dde6a75482e2d7c
冯坤 <928572909@qq.com>
2015-10-13 16:09:34 +0800

    no message

---
+++ ShouKeBao/Main/AppDelegate+Extend.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate+Extend.h
+//  ShouKeBao
+//
+//  Created by 冯坤 on 15/10/13.
+//  Copyright (c) 2015年 shouKeBao. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate (Extend)
+
+-(void)setStartAnamation;
+
+@end
