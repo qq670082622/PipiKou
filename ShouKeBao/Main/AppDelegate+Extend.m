bcf867c1e464ac07499367bc38e7183341a735b3
zhangzhengmei <13478644835@163.com>
2015-10-13 19:00:09 +0800

    启动动画

--- ShouKeBao/Main/AppDelegate+Extend.m
+++ ShouKeBao/Main/AppDelegate+Extend.m
@@ -13,30 +13,70 @@
 #define sixPSize ([UIScreen mainScreen].bounds.size.height > 668)
 
 
+
 @implementation AppDelegate (Extend)
 
 -(void)setStartAnamation{
     UIImageView * luanchImage = [[UIImageView alloc]initWithFrame:[[UIScreen mainScreen] bounds]];
+    
+    UIImageView *longLine = [[UIImageView alloc]init];
+    UIImageView *yuandian = [[UIImageView alloc]initWithFrame:CGRectMake(0, -4, 10, 10)];
+    
     if (foureSize) {
+        longLine.frame = CGRectMake([UIScreen mainScreen].bounds.size.width/8, [UIScreen mainScreen].bounds.size.height/2+15, [UIScreen mainScreen].bounds.size.width*3/4, 2);
         luanchImage.image = [UIImage imageNamed:@"4start"];
-    }else if(fiveSize){
-        luanchImage.image = [UIImage imageNamed:@"5start"];
-    }else if (sixSize){
-        luanchImage.image = [UIImage imageNamed:@"6start"];
-    }else if(sixPSize){
-        luanchImage.image = [UIImage imageNamed:@"6pstart"];
+        longLine.image = [UIImage imageNamed:@"changxian4_5"];
+        yuandian.image = [UIImage imageNamed:@"yuandian4_5"];
+    
+    }else{
+        
+        longLine.frame = CGRectMake([UIScreen mainScreen].bounds.size.width/8, [UIScreen mainScreen].bounds.size.height/2, [UIScreen mainScreen].bounds.size.width*3/4, 2);
+        if(fiveSize){
+            luanchImage.image = [UIImage imageNamed:@"5start"];
+            longLine.image = [UIImage imageNamed:@"changxian4_5"];
+            yuandian.image = [UIImage imageNamed:@"yuandian4_5"];
+            
+        }else if (sixSize){
+            luanchImage.image = [UIImage imageNamed:@"6start"];
+            longLine.image = [UIImage imageNamed:@"changxian_6"];
+            yuandian.image = [UIImage imageNamed:@"yuandian_6"];
+            
+        }else if(sixPSize){
+            luanchImage.image = [UIImage imageNamed:@"6pstart"];
+            longLine.image = [UIImage imageNamed:@"changxian_6p"];
+            yuandian.image = [UIImage imageNamed:@"yuandian_6p"];
+        }
+        
     }
+    
+    
     luanchImage.userInteractionEnabled = YES;
     [self.window addSubview:luanchImage];
     
+    [luanchImage addSubview:longLine];
+    [longLine addSubview:yuandian];
+
+
     //加载动画
-    [UIView animateWithDuration:1.0 animations:^{
-        luanchImage.alpha = 0;
+    [UIView animateWithDuration:2.0 animations:^{
+        yuandian.frame = CGRectMake([UIScreen mainScreen].bounds.size.width*3/4, -4, 10, 10);
     } completion:^(BOOL finished) {
-        [luanchImage removeFromSuperview];
+        [UIView animateWithDuration:0.5 animations:^{
+            luanchImage.alpha = 0;
+        } completion:^(BOOL finished) {
+            [luanchImage removeFromSuperview];
+        }];
     }];
+    
+    
+    
 
 }
+
+- (void)value{
+    
+}
+
 
 
 @end
