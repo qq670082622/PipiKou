1b6191250d33cfe6f161969eb5365f74d8a86124
zhangzhengmei <13478644835@163.com>
2015-12-04 11:00:10 +0800

    no message

--- ShouKeBao/ShouKebao/Double12/DoubleModel.m
+++ ShouKeBao/ShouKebao/Double12/DoubleModel.m
@@ -10,26 +10,28 @@
 #import "NSMutableDictionary+QD.h"
 @implementation DoubleModel
 
-//-(void)encodeWithCoder:(NSCoder *)aCoder{
-//    [aCoder encodeObject:self.FirstTitle forKey:@"FirstTitle"];
-//    [aCoder encodeObject:self.SecondTitle forKey:@"SecondTitle"];
-//    [aCoder encodeObject:self.ThirdTitle forKey:@"ThirdTitle"];
-//    [aCoder encodeObject:self.CreatedDate forKey:@"CreatedDate"];
-//    [aCoder encodeObject:self.BannerUrl forKey:@"BannerUrl"];
-//    [aCoder encodeObject:self.LinkUrl forKey:@"LinkUrl"];
-//}
-//-(id)initWithCoder:(NSCoder *)aDecoder{
-//    self = [super init];
-//    if (self) {
-//        self.FirstTitle = [aDecoder decodeObjectForKey:@"FirstTitle"];
-//        self.SecondTitle = [aDecoder decodeObjectForKey:@"SecondTitle"];
-//        self.ThirdTitle = [aDecoder decodeObjectForKey:@"ThirdTitle"];
-//        self.CreatedDate = [aDecoder decodeObjectForKey:@"CreatedDate"];
-//        self.BannerUrl = [aDecoder decodeObjectForKey:@"BannerUrl"];
-//        self.LinkUrl = [aDecoder decodeObjectForKey:@"LinkUrl"];
-//    }
-//    return self;
-//}
+-(void)encodeWithCoder:(NSCoder *)aCoder{
+    [aCoder encodeObject:self.FirstTitle forKey:@"FirstTitle"];
+    [aCoder encodeObject:self.SecondTitle forKey:@"SecondTitle"];
+    [aCoder encodeObject:self.ThirdTitle forKey:@"ThirdTitle"];
+    [aCoder encodeObject:self.CreatedDate forKey:@"CreatedDate"];
+    [aCoder encodeObject:self.BannerUrl forKey:@"BannerUrl"];
+    [aCoder encodeObject:self.LinkUrl forKey:@"LinkUrl"];
+    [aCoder encodeObject:self.IconUrl forKey:@"IconUrl"];
+}
+-(id)initWithCoder:(NSCoder *)aDecoder{
+    self = [super init];
+    if (self) {
+        self.FirstTitle = [aDecoder decodeObjectForKey:@"FirstTitle"];
+        self.SecondTitle = [aDecoder decodeObjectForKey:@"SecondTitle"];
+        self.ThirdTitle = [aDecoder decodeObjectForKey:@"ThirdTitle"];
+        self.CreatedDate = [aDecoder decodeObjectForKey:@"CreatedDate"];
+        self.BannerUrl = [aDecoder decodeObjectForKey:@"BannerUrl"];
+        self.LinkUrl = [aDecoder decodeObjectForKey:@"LinkUrl"];
+        self.IconUrl = [aDecoder decodeObjectForKey:@"IconUrl"];
+    }
+    return self;
+}
 
 + (instancetype)modalWithDict:(NSDictionary *)dic{
     return [[self alloc]initWithDict:dic];
@@ -39,6 +41,7 @@
     
     if (self = [super init]) {
          NSMutableDictionary *dic = [NSMutableDictionary cleanNullResult:dictionary];
+        self.IconUrl = dic[@"IconUrl"];
         self.BannerUrl = dic[@"BannerUrl"];
         self.CreatedDate = dic[@"CreatedDate"];
         self.FirstTitle = dic[@"FirstTitle"];
